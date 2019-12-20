76a492f7d5ff02b8edb4b3de6d735174e0c6a2f2
Vzhukov642 <vzukov642@gmail.com>
2019-12-20 17:18:55 +0300

    try add icons

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Item(models.Model):
-    name_item = models.TextField(verbose_name='Название вещи', max_length=200)
-    price_item = models.FloatField(verbose_name='Стоимость вещи', max_length=10)
-    url_icon = models.TextField(verbose_name='Ссылка на изображение', max_length=999, default= ' ')
+    name_item = models.TextField(verbose_name='Название вещи', max_length=200, db_index=True)
+    price_item = models.FloatField(verbose_name='Стоимость вещи', max_length=10, db_index=True)
+    url_icon = models.CharField(verbose_name='Ссылка на изображение', max_length=999, default= ' ', db_index=True)
 
     def __str__(self):
         return '%s %s' % (self.name_item, self.price_item)
--- articles/views.py
+++ articles/views.py
@@ -21,7 +21,8 @@ def index(request):
     for i in popular_items:
         item = Item(name_item = i.split(',,')[0], price_item = i.split(',,')[1], url_icon = 'https://cdn.csgo.com/item/'+ i.split(',,')[0] +'/100.png')
         item.save()
-    name_item = Item.objects.all()
+    name_item = Item.objects.values_list('name_item')
+    url_icons = Item.objects.values_list('url_icon')
     count_items = len(name_item)
     date_now = datetime.now()
     
@@ -29,10 +30,10 @@ def index(request):
         min_price = request.GET.get('min_value')
         max_price = request.GET.get('max_value')
         sort = True
-        sorted_price = Item.objects.raw('SELECT id, name_item FROM articles_item WHERE price_item BETWEEN %s AND %s', [min_price, max_price])
+        sorted_price = Item.objects.filter(price_item__range = [min_price, max_price])
         sorted_count_items = len(sorted_price)
         data = {'name': name_item, 'count_items' : count_items, 'sort_items_form': sort_items_form, 'sorted_items': sorted_price, 'sort': sort, 'sorted_items_price': sorted_count_items,'time_now': date_now}
         return render(request, 'index.html', data)
     else:
-        data = {'name_item': name_item, 'count_items' : count_items, 'sort_items_form': sort_items_form, 'time_now': date_now}
+        data = {'name_item': name_item, 'count_items': count_items, 'url_icon': url_icons, 'sort_items_form': sort_items_form, 'time_now': date_now}
         return render(request, 'index.html', data)
