00f46451f62b55affce9532b928551dc99abd361
Vzhukov642 <vzukov642@gmail.com>
2020-01-19 00:23:11 +0300

    Added count sell

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Item(models.Model):
-    name_item = models.CharField(verbose_name='Название вещи', max_length=400, db_index=True)
-    price_item = models.FloatField(verbose_name='Стоимость вещи', max_length=10, db_index=True)
+    name = models.CharField(verbose_name='Название вещи', max_length=400, db_index=True)
+    price = models.FloatField(verbose_name='Стоимость вещи', max_length=10, db_index=True)
     url_icon = models.CharField(verbose_name='Ссылка на изображение', max_length=999, default= ' ', db_index=True)
 
     def __str__(self):
-        return '%s %s' % (self.name_item, self.price_item)
+        return '%s %s' % (self.name, self.price)
 
     class Meta:
         verbose_name = 'Предмет'
--- articles/views.py
+++ articles/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 import requests
 from .models import Item
 from .forms import PriceSelection
+from django.db.models import Count
 items = []
-errors = []
 response_buy = requests.get('https://market.csgo.com/history/json/')
 
 
@@ -15,23 +15,19 @@ def index(request):
     for i in range(0, 50, 1):
         items.append(response_buy.json()[i]['hash_name'] + ',,' + str(response_buy.json()[i]['price']))
     items.sort()
-    popular_items = list(set(items))
+    popular_items = list(items)
     for i in popular_items:
-        item = Item(name_item=i.split(',,')[0], price_item=i.split(',,')[1], url_icon='https://cdn.csgo.com/item/'+ i.split(',,')[0] +'/300.png')
+        item = Item(name=i.split(',,')[0], price=i.split(',,')[1], url_icon='https://cdn.csgo.com/item/'+ i.split(',,')[0] +'/300.png')
         item.save()
-    name_item = Item.objects.all()
-    count_items = len(name_item)
+    counts_item = Item.objects.values('name', 'price', 'url_icon').annotate(sell_count=Count('name')).distinct()
     date_now = datetime.now()
     if request.GET:
+        sort = True
+        date_now = datetime.now()
         min_price = request.GET.get('min_value')
         max_price = request.GET.get('max_value')
-        sort = True
-        sorted_price = Item.objects.filter(price_item__range=[min_price, max_price])
-        sorted_count_items = len(sorted_price)
-        data = {'name': name_item, 'sort_items_form': sort_items_form,
-                'sorted_items': sorted_price, 'sort': sort,
-                'sorted_items_price': sorted_count_items, 'time_now': date_now}
+        sorted_price = Item.objects.filter(price__range=[min_price, max_price])
+        data = {'sorted_items': sorted_price, 'sort': sort, 'time_now': date_now}
         return render(request, 'index.html', data)
-    data = {'items': name_item, 'count_items': count_items,
-            'sort_items_form': sort_items_form, 'time_now': date_now}
+    data = {'sort_items_form': sort_items_form, 'time_now': date_now, 'counts_item': counts_item}
     return render(request, 'index.html', data)
