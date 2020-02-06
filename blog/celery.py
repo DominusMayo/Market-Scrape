a1e17214181373905eba49a3d8e145796d6cf731
vzukov642 <vzukov642@gmail.com>
2020-02-06 21:42:25 +0300

    fix error blog.settings

--- blog/celery.py
+++ blog/celery.py
@@ -1,7 +1,7 @@
 from celery import Celery
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 app = Celery('blog')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
